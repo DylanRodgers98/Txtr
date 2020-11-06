d591f3186674c4cf461537af558b08ca522c4c55
Dylan Rodgers <dylanirodgers@aol.com>
2020-11-06 12:42:05 +0000

    Fix relationship between a User and their Posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,25 +11,25 @@ class Post extends Model
 
     public function user()
     {
-        return $this->hasOne('App\Models\User');
+        return $this->belongsTo(User::class);
     }
 
     public function comments()
     {
-        return $this->hasMany('App\Models\Post', 'parent_post_id');
+        return $this->hasMany(Post::class, 'parent_post_id');
     }
 
     /**
      * A Post may be a 'comment', which has the same properties as a Post but
-     * is a reply to another Post or comment, and so has a reference to a 
-     * 'parent' Post. A Post with no parent Post is therefore not a comment. 
-     * 
+     * is a reply to another Post or comment, and so has a reference to a
+     * 'parent' Post. A Post with no parent Post is therefore not a comment.
+     *
      * @return Illuminate\Database\Eloquent\Relations\BelongsTo the BelongsTo
      * part of a one-to-many relationship between a Post and its comments, who
      * themselves are also Posts.
      */
     public function parentPost()
     {
-        return $this->belongsTo('App\Models\Post');
+        return $this->belongsTo(Post::class);
     }
 }
