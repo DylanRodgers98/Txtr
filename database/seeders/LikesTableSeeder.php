1e2e3bf99b7ea6d35813839ec17f9ff53d79274e
Dylan Rodgers <dylanirodgers@aol.com>
2020-11-08 15:45:06 +0000

    Move Likes seeding back to standalone seeder

---
+++ database/seeders/LikesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class LikesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach (Post::get() as $post) {
+            $numberOfUsers = rand(0, 10);
+            $randomUserIDs = User::inRandomOrder()->where('id', '<>', $post->user->id)->limit($numberOfUsers)->pluck('id');
+            $post->likedBy()->attach($randomUserIDs);
+        }
+    }
+}
--- database/seeders/PostTableSeeder.php
+++ database/seeders/PostTableSeeder.php
@@ -19,32 +19,7 @@ class PostTableSeeder extends Seeder
         $post->user_id = User::where('name', 'Dylan Rodgers')->value('id');
         $post->body = "This is my first post!";
         $post->save();
-        $this->seedLikesForPost($post);
 
-        /**
-         * Using a for-loop rather than count(50) on factory because create()
-         * creates the required amount of records using the definition in the
-         * factory and THEN persists them to the database. This means that
-         * within the factory the parent_post_id will only ever be the id of
-         * $post created above, as it will be the only Post persisted in the
-         * database at the time of creation. By creating one Post at a time,
-         * its id then becomes available for future Posts created by the
-         * factory to reference as a foreign key for parent_post_id.
-         * This for-loop also seeds 'likes' for each created Post. (Note: this
-         * could be done within an afterCreating callback configured within the
-         * factory, however doing this outside of the factory actually performs
-         * around 200-300ms faster.)
-         */
-        for ($i = 0; $i < 50; $i++) {
-            $postFromFactory = Post::factory()->createOne();
-            $this->seedLikesForPost($postFromFactory);
-        }
-    }
-
-    private function seedLikesForPost(Post $post)
-    {
-        $numberOfUsers = rand(0, 10);
-        $randomUserIDs = User::inRandomOrder()->limit($numberOfUsers)->pluck('id');
-        $post->likedBy()->attach($randomUserIDs);
+        Post::factory()->count(50)->create();
     }
 }
