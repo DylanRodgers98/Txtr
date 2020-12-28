e0da643cb025d72804933d90f67ecfe243c0e7e4
Dylan Rodgers <dylanirodgers@aol.com>
2020-12-28 15:29:40 +0000

    Extract post html into its own component

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,25 +1,7 @@
 @section('title', ($post->user->profile->display_name ?? $user->username) . "'s Post")
 
 <x-app-layout>
-    <div class="py-1">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <a href="{{ route('users.show', ['user' => $post->user]) }}">
-                        <img src="{{ $post->user->profile->profile_img_path ?? "https://i.stack.imgur.com/l60Hf.png" }}"
-                            class="float-left mr-4 rounded-full h-12 w-12"
-                            alt="{{ $post->user->profile->display_name }}'s Profile Picture">
-                        <b>{{ $post->user->profile->display_name ?? $post->user->username }}</b>
-                        <i>{{ '@' . $post->user->username }}</i>
-                        {{ ' · ' . $post->created_at }}
-                    </a>
-                    <div>
-                        <a href="{{ route('posts.show', ['post' => $post]) }}">{{ $post->body }}</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-post-component :post="$post"/>
 
     {{-- <ul>
         <li>User: <a href="{{ route('users.show', ['user' => $post->user]) }}">{{ $post->user->profile->display_name ?? $user->username }}</a></li>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,12 +1,6 @@
-@section('title', $user->profile->display_name ?? $user->username)
+@section('title', $user->profile->display_name . " (@" . $user->username . ")")
 
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ $user->profile->display_name ?? $user->username }}
-        </h2>
-    </x-slot>
-
     <img src="{{ $user->profile->banner_img_path ?? "https://pbs.twimg.com/profile_banners/1029010076/1520800849/1500x500" }}"
         class="w-full h-96 sm:px-6 lg:px-8" alt="{{ $user->profile->display_name }}'s Banner">
 
@@ -17,7 +11,17 @@
                     class="h-64 w-64 p-4 rounded-full float-left"
                     alt="{{ $user->profile->display_name }}'s Profile Picture">
 
-                <div class="py-4">
+                <div class="p-4">
+                    <button class="float-right bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                        @if ($isAuthUser)
+                            Edit Profile
+                        @elseif ($isAuthUserFollowing)
+                            Unfollow
+                        @else
+                            Follow
+                        @endif
+                    </button>
+
                     <div>
                         <b>{{ $user->profile->display_name ?? $user->username }}</b>
                     </div>
@@ -39,33 +43,20 @@
                         @endif
                     </div>
 
-                    <b>{{ $user->following->count() }}</b> Following
-                    <b>{{ $user->followers->count() }}</b> Followers
+                    <a href="{{ route('users.indexFollowing', ['user' => $user]) }}">
+                        <b>{{ $user->following->count() }}</b> Following
+                    </a>
+                    <a href="{{ route('users.indexFollowers', ['user' => $user]) }}">
+                        <b>{{ $user->followers->count() }}</b> Followers
+                    </a>
                 </div>
             </div>
         </div>
     </div>
 
-    <ul>
+    <ul class="pb-1">
         @foreach ($user->posts as $post)
-            <div class="py-1">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div class="p-6 bg-white border-b border-gray-200">
-                            <a href="{{ route('users.show', ['user' => $post->user]) }}">
-                                <img src="{{ $post->user->profile->profile_img_path ?? "https://i.stack.imgur.com/l60Hf.png" }}"
-                                    class="float-left mr-4 rounded-full h-12 w-12"
-                                    alt="{{ $post->user->profile->display_name }}'s Profile Picture">
-                                <b>{{ $post->user->profile->display_name ?? $post->user->username }}</b>
-                                <i>{{ '@' . $post->user->username }}</i>
-                            </a>
-                            <div>
-                                <a href="{{ route('posts.show', ['post' => $post]) }}">{{ $post->body }}</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <x-post-component :post="$post"/>
         @endforeach
     </ul>
 </x-app-layout>
