3177cce0a435e290e676aef62c7b0ebf381a1a9f
Dylan Rodgers <dylanirodgers@aol.com>
2020-12-28 12:23:27 +0000

    Display profile image and handle on posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -20,10 +20,16 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <b><a href="{{ route('users.show', ['user' => $post->user]) }}">
-                            {{ $post->user->profile->display_name ?? $post->user->username }}</a>:</b>
-                        <a href="{{ route('posts.show', ['post' => $post]) }}">
-                            {{ $post->body }}</a>
+                        <a href="{{ route('users.show', ['user' => $post->user]) }}">
+                            <img src="{{ $post->user->profile->profile_img_path ?? "https://i.stack.imgur.com/l60Hf.png" }}"
+                                class="float-left mr-4 rounded-full h-12 w-12"
+                                alt="{{ $post->user->profile->display_name }}'s Profile Picture">
+                            <b>{{ $post->user->profile->display_name ?? $post->user->username }}</b>
+                            <i>{{ '@' . $post->user->username }}</i>
+                        </a>
+                        <div>
+                            <a href="{{ route('posts.show', ['post' => $post]) }}">{{ $post->body }}</a>
+                        </div>
                     </div>
                 </div>
             </div>
