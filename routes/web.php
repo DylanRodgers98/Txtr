56853e046b7befb8fb9ecfd77e0c7ccb7e279258
Dylan Rodgers <dylanirodgers@aol.com>
2021-01-04 09:11:08 +0000

    Use class resolution for registering NewsAPI in service container

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 /**
  * Register NewsApi
  */
-app()->singleton('newsApi', function ($app) {
+app()->singleton(NewsApi::class, function ($app) {
     return new NewsApi();
 });
 
